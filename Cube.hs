77d180bbf4cb33343558ff97546558ad3c6d405b
Jay Adkisson <jjmadkisson@gmail.com>
2012-08-17 17:57:30 +0200

    init

---
+++ Cube.hs
@@ -0,0 +1,9 @@
+--
+-- Copyright (c) 2012 Jay Adkisson - http://jayferd.us/
+-- MIT License
+--
+import System.Environment
+
+-- | 'main' runs the main program
+main :: IO ()
+main = println "Hello, World!"
