e0fb8ab806b172f85c7f586f6b449743afb47a1f
Jay Adkisson <jjmadkisson@gmail.com>
2012-08-19 15:51:16 +0200

    rotations, middle slices, and thick turns

--- Data/Cube.hs
+++ Data/Cube.hs
@@ -34,21 +34,22 @@ module Data.Cube (
     printCube,
 
   -- turns
-    Turn(NullTurn, Forward, Reverse, Double, Multi),
-    readTurn,
-    showTurn,
-    applyTurn,
+  --  Turn(NullTurn, Forward, Reverse, Double, Multi),
+  --  readTurn,
+  --  showTurn,
+  --  applyTurn,
 
   -- algs
-    sexy,
-    tperm,
-    sune
+  --  sexy,
+  --  tperm,
+  --  sune
   ) where
 
   import Data.Monoid
   import Data.List (intersperse, (\\), sort)
   -- import qualified Text.ParserCombinators.Parsec as Parsec
   import Text.ParserCombinators.Parsec
+  import Data.Char (toUpper, toLower)
 
   {- COLORS -}
   data Color = White | Yellow | Red | Orange | Green | Blue
@@ -88,6 +89,14 @@ module Data.Cube (
   data Face = L | R | F | B | U | D deriving(Show, Eq, Read, Ord)
   allFaces = [L, R, F, B, U, D]
 
+  initColor :: Face -> Color
+  initColor U = White
+  initColor D = Yellow
+  initColor F = Green
+  initColor B = Blue
+  initColor R = Red
+  initColor L = Orange
+
   oppositeFace :: Face -> Face
   oppositeFace L = R
   oppositeFace R = L
@@ -107,20 +116,18 @@ module Data.Cube (
     where
       nextCycle = tail $ dropWhile (/= face) $ cycle (faceRing direction)
 
-  initColor :: Face -> Color
-  initColor U = White
-  initColor D = Yellow
-  initColor F = Green
-  initColor B = Blue
-  initColor R = Red
-  initColor L = Orange
-
   rotateFace :: Face -> Face -> Face
   rotateFace direction face
     -- noop if the rotating face is the same face or the opposite
     | direction == face                 = face
     | direction == oppositeFace face    = face
     | otherwise                         = head $ nextFaceRing direction face
+
+  mirrorFace :: Face -> Face -> Face
+  mirrorFace mirror face
+    | mirror == face              = oppositeFace face
+    | mirror == oppositeFace face = oppositeFace face
+    | otherwise                   = face
 
   {- ORIENTATIONS -}
   data Orientation = Orientation { topFace :: Face, frontFace :: Face }
@@ -135,8 +142,8 @@ module Data.Cube (
   faceOn B o = oppositeFace $ frontFace o
 
   rotateOrientation :: Face -> Orientation -> Orientation
-  rotateOrientation face (Orientation t f) = Orientation (rotate t) (rotate f)
-    where rotate = rotateFace (oppositeFace face)
+  rotateOrientation face o@(Orientation t f) = Orientation (rotate t) (rotate f)
+    where rotate = rotateFace $ faceOn (oppositeFace face) o
 
   {- CUBIES -}
 
@@ -189,6 +196,9 @@ module Data.Cube (
     where
       newCubies = map (rotateCubie face) cubies
       newOrientation = rotateOrientation face orientation
+
+  getOrientation :: Cube -> Orientation
+  getOrientation (Cube _ o) = o
 
   initCube :: Cube
   initCube = Cube (initEdges ++ initCorners) initOrientation
@@ -265,11 +275,25 @@ module Data.Cube (
       concatColors = foldl1 (zipWith (++))
 
   {- TURNS -}
-  data Turn = NullTurn
-            | Forward Face
-            | Reverse Face
-            | Double Face
-            | Multi Turn Turn
+  data Direction = Forward | Reverse | Double
+  data Operation = Turn | Rotate | Thick | Middle
+  data Move = Move Operation Direction Face
+  newtype Alg = Alg [Move]
+
+  oppositeDirection :: Direction -> Direction
+  oppositeDirection Double = Double
+  oppositeDirection Forward = Reverse
+  oppositeDirection Reverse = Forward
+
+  applyOperation :: Operation -> Face -> Cube -> Cube
+  applyOperation op f = case op of
+    Turn   -> turnForward f
+    Rotate -> rotateCube f
+    Thick  -> turnThick f
+    Middle -> turnThick f . turnReverse f
+    where
+      turnReverse f = doTimes 3 (turnForward f)
+      turnThick f = rotateCube f . turnForward (oppositeFace f)
 
   turnForward :: Face -> Cube -> Cube
   turnForward f (Cube cubies o) =
@@ -279,69 +303,199 @@ module Data.Cube (
         | onFace f cubie = rotateCubie f cubie
         | otherwise      = cubie
 
-  applyTurn :: Turn -> Cube -> Cube
-  applyTurn turn = case turn of
-    NullTurn    -> id
-    Forward f   -> turnForward f
-    Double f    -> turnForward f . turnForward f
-    Reverse f   -> turnForward f . turnForward f . turnForward f
-    Multi t1 t2 -> applyTurn t2 . applyTurn t1
+  doTimes :: Int -> (a -> a) -> (a -> a)
+  doTimes n = foldl1 (.) . replicate n
 
-  applyTurns :: [Turn] -> Cube -> Cube
-  applyTurns = applyTurn . mconcat
+  applyMove :: Move -> Cube -> Cube
+  applyMove (Move op dir face) = case dir of
+    Forward -> doMove
+    Double  -> doTimes 2 doMove
+    Reverse -> doTimes 3 doMove
+    where
+      doMove = applyOperation op face
 
-  instance Monoid Turn where
-    mappend = Multi
-    mempty = NullTurn
+  applyAlg :: Alg -> Cube -> Cube
+  applyAlg (Alg moves) cube = foldl (flip applyMove) cube moves
 
-  showTurn :: Turn -> String
-  showTurn turn = case turn of
-    NullTurn    -> ""
-    Forward f   -> show f
-    Reverse f   -> show f ++ "'"
-    Double f    -> show f ++ "2"
-    Multi t1 t2 -> showTurn t1 ++ showTurn t2
+  rotateMove :: Face -> Move -> Move
+  rotateMove face (Move op dir moveFace) =
+    Move op dir (rotateFace face moveFace)
 
-  instance Show Turn where show = ("readTurn " ++) . show . showTurn
-  -- instance Read Turn where read = readTurn
+  rotateAlg :: Face -> Alg -> Alg
+  rotateAlg = mapAlg . rotateMove
 
-  readTurn :: String -> Turn
-  readTurn input = case parse parser "input" input of
+  mirrorMove :: Face -> Move -> Move
+  mirrorMove mirror (Move op dir face) =
+    Move op (oppositeDirection dir) (mirrorFace mirror face)
+
+  mirrorAlg :: Face -> Alg -> Alg
+  mirrorAlg = mapAlg . mirrorMove
+
+  mirror = mirrorAlg R
+
+  orientMove :: Orientation -> Move -> Move
+  orientMove o (Move op dir face) = Move op dir (faceOn face o)
+
+  orientAlg :: Orientation -> Alg -> Alg
+  orientAlg = mapAlg . orientMove
+
+  mapAlg :: (Move -> Move) -> Alg -> Alg
+  mapAlg f (Alg moves) = Alg (map f moves)
+
+  inverseMove :: Move -> Move
+  inverseMove (Move op dir face) = Move op (oppositeDirection dir) face
+
+  inverse :: Alg -> Alg
+  inverse (Alg moves) = Alg $ map inverseMove $ reverse moves
+
+  showMove :: Move -> String
+  showMove (Move op dir face) = case op of
+    Turn  -> show face ++ showDirection dir
+    Thick -> map toLower (show face) ++ showDirection dir
+    Rotate ->
+      case face of
+           R -> "x" ++ showDirection dir
+           U -> "y" ++ showDirection dir
+           F -> "z" ++ showDirection dir
+
+           -- these won't be generated by readAlg, but may
+           -- be created by rotateMove, so we still have to
+           -- show them correctly.
+           L -> "x" ++ showDirection (oppositeDirection dir)
+           D -> "y" ++ showDirection (oppositeDirection dir)
+           B -> "z" ++ showDirection (oppositeDirection dir)
+
+    Middle ->
+      case face of
+           L -> "M" ++ showDirection dir
+           R -> "M" ++ showDirection (oppositeDirection dir)
+
+           D -> "E" ++ showDirection dir
+           U -> "E" ++ showDirection (oppositeDirection dir)
+
+           F -> "S" ++ showDirection dir
+           B -> "S" ++ showDirection (oppositeDirection dir)
+
+
+  showDirection :: Direction -> String
+  showDirection dir = case dir of
+    Forward -> ""
+    Reverse -> "'"
+    Double  -> "2"
+
+  showAlg :: Alg -> String
+  showAlg (Alg moves) = concat . intersperse " " . map showMove $ moves
+
+  instance Show Alg where show = ("readAlg " ++) . show . showAlg
+
+  readAlg :: String -> Alg
+  readAlg input = case parse parser "input" input of
     Left _       -> undefined
     Right result -> result
     where
-      parser = fmap mconcat $ many $ do
-        face <- parseFace
-        modifier <- parseModifier
-        return $ modifier face
+      parser = fmap Alg $ many $ do
+        spaces
+        (op, face) <- parseLetter
+        dir <- parseDirection
+        spaces
+        return $ Move op dir face
         where
-          parseFace = fmap (read . return) $ oneOf $ map (head . show) allFaces
-          parseModifier = (char '\'' >> return Reverse)
-                      <|> (char '2'  >> return Double)
-                      <|> (return Forward)
+          faceLetters = map (head . show) allFaces
+
+          parseTurn :: Parser (Operation, Face)
+          parseTurn = do
+            face <- fmap (read . return) $ oneOf $ faceLetters
+            return (Turn, face)
+
+          parseThick :: Parser (Operation, Face)
+          parseThick = do
+            face <- fmap (read . return . toUpper) $ oneOf $ map toLower faceLetters
+            return (Thick, face)
+
+          parseRotation :: Parser (Operation, Face)
+          parseRotation = do
+            face <- (char 'x' >> return R)
+                <|> (char 'y' >> return U)
+                <|> (char 'z' >> return F)
+
+            return (Rotate, face)
+
+          parseMiddle :: Parser (Operation, Face)
+          parseMiddle = do
+            face <- (char 'M' >> return L)
+                <|> (char 'E' >> return D)
+                <|> (char 'S' >> return F)
+
+            return (Middle, face)
+
+          parseLetter :: Parser (Operation, Face)
+          parseLetter = parseTurn
+                    <|> parseRotation
+                    <|> parseThick
+                    <|> parseMiddle
+
+          parseDirection :: Parser Direction
+          parseDirection = (char '\'' >> return Reverse)
+                       <|> (char '2'  >> return Double)
+                       <|> (return Forward)
+
+  instance Monoid Alg where
+    mempty = Alg []
+    mappend (Alg xs) (Alg ys) = Alg (xs ++ ys)
 
   infixr 5 +++
-  (+++) :: Turn -> Turn -> Turn
+  (+++) :: (Monoid a) => a -> a -> a
   (+++) = mappend
 
-  result :: Turn -> Cube
-  result turn = applyTurn turn initCube
+  result :: Alg -> Cube
+  result alg = applyAlg alg initCube
 
-  inverse :: Turn -> Turn
-  inverse turn = case turn of
-    NullTurn    -> NullTurn
-    Forward f   -> Reverse f
-    Reverse f   -> Forward f
-    Double f    -> Double f
-    Multi t1 t2 -> Multi (inverse t2) (inverse t1)
+  solveCase :: Alg -> Cube
+  solveCase = result . inverse
 
-  turnSeq :: Turn -> [Turn]
-  turnSeq NullTurn    = []
-  turnSeq (Multi t1 t2) = turnSeq t1 ++ turnSeq t2
-  turnSeq x = [x]
+  normalize :: Alg -> Alg -> Alg
+  normalize a b = a +++ b +++ inverse a
 
-  sexy = readTurn "RUR'U'"
-  tperm = sexy +++ readTurn "R'FR2U'R'U'RUR'F'"
-  sune = readTurn "RUR'URU2R'"
+  intertwine :: Alg -> Alg -> Alg
+  intertwine a b = normalize a b +++ inverse b
 
-  -- TODO: rotations, thick turns, etc.
+  sexy = intertwine (readAlg "R") (readAlg "U")
+
+  startT = intertwine (readAlg "R") (readAlg "U")
+       +++ intertwine (readAlg "R'") (readAlg "F")
+
+  endT = readAlg "FRU'R'U' RUR'F'"
+
+  tperm = startT +++ endT
+  yperm = endT +++ startT
+  uperm = readAlg "R'U R'U'  R'U' R'U  RUR2"
+
+  vperm = readAlg "R'U R'd' R'F' R2U' R'U R'F RF y'"
+
+  zperm = readAlg "M2U' M2U' M'U2 M2U2 M'U2"
+  hperm = half +++ (readAlg "U2") +++ half
+    where half = normalize (readAlg "R2U2") (readAlg "R")
+
+  jperm = readAlg "RU2R'U' RU2L'U R'U'L"
+
+  gperm = readAlg "RUR' y' R2u'RU'R'UR'u R2 y"
+
+  sune = readAlg "RUR'U RU2R'"
+
+  {- NORMALIZATION -}
+  -- turns a single move into an array of Turns and Rotations
+  normalizeMove :: Move -> [Move]
+  normalizeMove move@(Move op dir face) = case op of
+    Thick -> [ Move Rotate dir face
+             , Move Turn dir (oppositeFace face)
+             ]
+    Middle -> [ Move Rotate dir face
+              , Move Turn dir (oppositeFace face)
+              , Move Turn (oppositeDirection dir) face
+              ]
+    _ -> [move]
+
+  normalizeAlg :: Alg -> Alg
+  normalizeAlg (Alg moves) = Alg $ do
+    move <- moves
+    normalizeMove move
